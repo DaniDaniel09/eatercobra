96531e41d04d0f28e80e96a22eab0887fe17ec26
DaniDaniel09 <114660597+DaniDaniel09@users.noreply.github.com>
2023-04-21 09:38:30 +0200

    Add files via upload

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ask.game"},{"l":"com.ask.game.constants"},{"l":"com.ask.game.dto"},{"l":"com.ask.game.objects"},{"l":"com.ask.game.util"}];updateSearchResults();
